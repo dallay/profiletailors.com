b6c6893817c12c7b8963a166b9ca2832aea75887
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2026-09-10 19:29:25 +0200

    refactor(smp): migrate Modulith metadata to Kotlin (#1010)

--- server/smp/src/main/kotlin/com/profiletailors/smp/identity/package-info.java
+++
@@ -1,6 +0,0 @@
-@org.springframework.modulith.ApplicationModule(
-    allowedDependencies = {"authorization :: domain", "tenancy :: application", "audit :: domain", "governance :: application", "credentials :: application"},
-    displayName = "Identity"
-)
-@org.springframework.modulith.NamedInterface("application")
-package com.profiletailors.smp.identity;
